c7512c73b32330fe24e612c6768d0705a0906882
Hi928 <hiroki.i928@gmail.com>
2023-04-30 08:16:38 +0900

    🔧 prettier、scriptsの編集

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "no-sparse-arrays": "off",
+    "no-var": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    "plugins": [
-        "react"
-    ],
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
-        "prettier"
-    ],
-    "rules": {
-        "react/prop-types": "off"
-    },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    }
+  },
 };
